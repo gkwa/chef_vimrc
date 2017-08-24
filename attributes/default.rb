17c7e872ac63b2ea4e6ef6b58e9039920bc310f6
Taylor Monacelli <taylormonacelli@gmail.com>
2017-08-23 20:10:46 -0400

    organize

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
+default['chef_vimrc']['vim']['initfile'] = 'vimrc'
+default['chef_vimrc']['nvim']['initfile'] = 'init.vim'
+
 default['chef_vimrc']['vim']['homedir'] = ENV['HOME'].tr('\\','/')
 default['chef_vimrc']['nvim']['homedir'] = case node['platform']
                                           when 'windows'
@@ -5,8 +8,6 @@ default['chef_vimrc']['nvim']['homedir'] = case node['platform']
                                           else
                                             "#{ENV['HOME']}/.config".tr('\\','/')
                                           end
-default['chef_vimrc']['vim']['initfile'] = 'vimrc'
-default['chef_vimrc']['nvim']['initfile'] = 'init.vim'
 
 default['chef_vimrc']['vim']['basedir'] = case node['platform']
                                           when 'windows'
