359c5719703e0e9c8e42e4e2c72c488a6eabfadf
Taylor Monacelli <taylormonacelli@gmail.com>
2017-11-02 08:39:45 -0700

    Fix FATAL: Chef::Exceptions::ChildConvergeError: Chef run process
    Recipe: chef_vimrc::taylorconfig
      * directory[/root/.vim/plugged] action create (up to date)
        * template[/root/.vim/vimrc] action create (up to date)
    	  * execute[PlugInstall] action run[2017-11-02T15:34:21+00:00]
    	  FATAL: Chef::Exceptions::ChildConvergeError: Chef run process
    	  terminated by signal 9 (KILL)

--- recipes/taylorconfig.rb
+++ recipes/taylorconfig.rb
@@ -16,27 +16,27 @@ include_recipe 'chef_vimrc::default'
     powershell_script 'Install and Update plugins' do
       code <<-'EOH'
       if(test-path('C:\\Program Files\\vim\\vim80\\vim.exe')){
-        & 'C:\\Program Files\\vim\\vim80\\vim.exe' slient! +VimEnter +PlugInstall +qall
-        & 'C:\\Program Files\\vim\\vim80\\vim.exe' slient! +VimEnter +PlugUpdate +qall
+        & 'C:\\Program Files\\vim\\vim80\\vim.exe' silent! +VimEnter +PlugInstall +qall
+        & 'C:\\Program Files\\vim\\vim80\\vim.exe' silent! +VimEnter +PlugUpdate +qall
       }
       if(test-path('C:\\Program Files (x86)\\vim\\vim80\\vim.exe')){
-        & 'C:\\Program Files (x86)\\vim\\vim80\\vim.exe' slient! +VimEnter +PlugInstall +qall
-        & 'C:\\Program Files (x86)\\vim\\vim80\\vim.exe' slient! +VimEnter +PlugUpdate +qall
+        & 'C:\\Program Files (x86)\\vim\\vim80\\vim.exe' silent! +VimEnter +PlugInstall +qall
+        & 'C:\\Program Files (x86)\\vim\\vim80\\vim.exe' silent! +VimEnter +PlugUpdate +qall
       }
       if(test-path('C:\\tools\\neovim\\Neovim\\bin\\nvim.exe')){
-        & 'C:\\tools\\neovim\\Neovim\\bin\\nvim.exe' slient! +VimEnter +PlugInstall +qall
-        & 'C:\\tools\\neovim\\Neovim\\bin\\nvim.exe' slient! +VimEnter +PlugUpdate +qall
+        & 'C:\\tools\\neovim\\Neovim\\bin\\nvim.exe' silent! +VimEnter +PlugInstall +qall
+        & 'C:\\tools\\neovim\\Neovim\\bin\\nvim.exe' silent! +VimEnter +PlugUpdate +qall
       }
       EOH
       returns [0, 1]
     end
   else
     execute 'PlugInstall' do
-      command "#{whichvim} slient! +VimEnter +PlugInstall +qall"
+      command "#{whichvim} silent! +VimEnter +PlugInstall +qall"
     end
 
     execute 'PlugUpdate' do
-      command "#{whichvim} slient! +VimEnter +PlugUpdate +qall"
+      command "#{whichvim} silent! +VimEnter +PlugUpdate +qall"
     end
   end
 end
